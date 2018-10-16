691525653461d306532c1ded13e640d5d606fea8
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-10-16 12:17:48 +0900

    :white_check_mark: Add a test #4

---
+++ app/data/datastore/darksky_datastore_test.go
@@ -0,0 +1,36 @@
+package datastore_test
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/YutoMizutani/gohome/app/data/datastore"
+
+	"github.com/joho/godotenv"
+)
+
+func TestFetch(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping e2e tests in short mode.")
+	}
+
+	err := godotenv.Load("../../../.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	apiKey := os.Getenv("DARK_SKY_API_KEY")
+	latitude := os.Getenv("WEATHER_LATITUDE")
+	longitude := os.Getenv("WEATHER_LONGITUDE")
+	if apiKey == "nil" || latitude == "nil" || longitude == "nil" {
+		log.Fatal("Error not defined required env keys")
+	}
+
+	darkSkyDataStore := new(datastore.DarkSkyDataStore)
+	darkSkyDataStore.APIKey = apiKey
+	_, err = darkSkyDataStore.Fetch(latitude, longitude)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
