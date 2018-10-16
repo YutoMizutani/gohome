fbbc5b56b128e7fe0557102deafc7356b08a9e48
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-10-16 19:06:56 +0900

    :sparkles: Add builder and controller #4

---
+++ app/presenter/controller/weather_controller.go
@@ -0,0 +1,18 @@
+package controller
+
+import (
+	"github.com/YutoMizutani/gohome/app/domain/usecase"
+)
+
+type WeatherController struct {
+	Usecase usecase.WeatherUsecase
+}
+
+func (controller *WeatherController) Fetch(c Context) {
+	Weathers, err := controller.Usecase.Fetch()
+	if err != nil {
+		c.JSON(500, NewError(err))
+		return
+	}
+	c.JSON(200, Weathers)
+}
