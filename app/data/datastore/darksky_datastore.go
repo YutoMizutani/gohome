80b202acae1715eb5e182a799e5c07d21edb6f75
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-10-16 23:55:49 +0900

    :green_heart: Fix ci #4

--- app/data/datastore/darksky_datastore.go
+++ app/data/datastore/darksky_datastore.go
@@ -7,7 +7,10 @@ import (
 type DarkSkyDataStore struct {
 }
 
-func (dataStore *DarkSkyDataStore) Fetch(apiKey string, latitude string, longitude string) (res *forecast.Forecast, err error) {
+func (dataStore *DarkSkyDataStore) Fetch(
+	apiKey string,
+	latitude string,
+	longitude string) (res *forecast.Forecast, err error) {
 	res, err = forecast.Get(apiKey, latitude, longitude, "now", forecast.SI, "ja")
 	return
 }
