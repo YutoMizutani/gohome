4e2da9639c6a877bb9688d87548633bee701e333
YutoMizutani <yuto.mizutani.dev@gmail.com>
2018-10-16 11:49:13 +0900

    :art: Add env settings at main file #4

--- app/server.go
+++ app/server.go
@@ -4,9 +4,19 @@ import (
 	"log"
 
 	"github.com/YutoMizutani/gohome/app/infrastructure"
+
+	"github.com/joho/godotenv"
 )
 
+func prepareEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
 func main() {
+	prepareEnv()
 	err := infrastructure.Router.Run()
 	if err != nil {
 		log.Fatal(err)
